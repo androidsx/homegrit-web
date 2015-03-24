2c102aef7292379ea28c9426843e08ca0ec88830
Omar Pera <campbell.sx@gmail.com>
2015-03-24 11:20:45 -0400

    Integrate pundit for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Needed to include all the pundit helpers (authorization)
+  include Pundit
+
+  # Globally rescue Authorization Errors in controller.
+  # Returning 403 Forbidden if permission is denied
+  rescue_from Pundit::NotAuthorizedError, with: :permission_denied
+
+  private
+
+  def permission_denied
+    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+  end
 end
